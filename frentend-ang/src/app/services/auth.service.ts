2728fa02f0538ba525037f5a7348d6c681ec2e46
hanane <hananelahssini22@gmail.com>
2024-04-29 16:57:06 +0100

    second commit

---
+++ frentend-ang/src/app/services/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  public users:any={
+    admin:{password:'1234',roles:['STUDENTS,ADMIN']},
+    user1:{password:'1234',roles:['STUDENTS']}
+  }
+  public username:any;
+  public isAuthenticated:boolean=false;
+  public roles:[]=[];
+  constructor(private router:Router) { }
+  public login(username:string,password:string){
+    if(this.users[username] && this.users[username]['password']==password)
+    {this.username=username;
+    this.isAuthenticated=true;
+    this.roles=this.username[username]['roles'];
+    return true;}
+    else
+    return false;
+  }
+  logout() {
+    this.isAuthenticated=false;
+    this.roles=[];
+    this.username=undefined;
+    this.router.navigateByUrl('/login')
+  }
+}
